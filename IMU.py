d0cd4f494c8a1a707e8e24775d5a64ea140e12c4
fidelDenzel <fdenzelp@gmail.com>
2024-10-25 20:03:56 +0700

    imu calib 241025

--- IMU.py
+++ IMU.py
@@ -1,6 +1,7 @@
 import smbus					#import SMBus module of I2C
 import math
 import time         #import
+import statistics as stat
 
 bus = smbus.SMBus(1)
 Device_Address = 0x00
@@ -98,7 +99,70 @@ def complementary_filter(gyro_value, accel_value, dt, alpha = 0.98, value = 0.0)
 
 # Function to calculate accelerometer angles
 def get_accel_angle(acc_x, acc_y, acc_z):
+	roll = 0
+	pitch = 0
     # Calculate roll and pitch from accelerometer data
-    roll = math.atan2(acc_y, acc_z) * 180 / math.pi
-    pitch = math.atan(-acc_x / math.sqrt(acc_y * acc_y + acc_z * acc_z)) * 180 / math.pi
-    return roll, pitch
+	if (acc_x == 0 or acc_y == 0 or acc_z == 0):
+		roll = 0
+		pitch = 0
+	else:
+		roll = math.atan2(acc_y, acc_z) * 180 / math.pi
+		pitch = math.atan(-acc_x / math.sqrt(acc_y * acc_y + acc_z * acc_z)) * 180 / math.pi
+	return roll, pitch
+
+def roll_pitch_calibration(num_sample):
+	accel_offsets = {'x':0, 'y':0, 'z':0}
+	gyro_offsets = {'x':0, 'y':0, 'z':0}
+	roll_offsets = []
+	pitch_offsets = []
+
+	dt = 0.08
+	num_samples = 1000
+	
+	for _ in range(num_samples):
+		# Read raw accelerometer data
+		acc_x = read_raw_data(0x3B)
+		acc_y = read_raw_data(0x3D)
+		acc_z = read_raw_data(0x3F)
+		
+		# Read raw gyroscope data
+		gyro_x = read_raw_data(0x43)
+		gyro_y = read_raw_data(0x45)
+		gyro_z = read_raw_data(0x47)
+
+		# Convert gyroscope data to degrees per second
+		gyro_x = gyro_x / 131.0
+		gyro_y = gyro_y / 131.0
+		gyro_z = gyro_z / 131.0
+
+		accel_offsets['x'] += acc_x
+		accel_offsets['y'] += acc_y
+		accel_offsets['z'] += acc_z
+		
+		gyro_offsets['x'] += gyro_x
+		gyro_offsets['y'] += gyro_y
+		gyro_offsets['z'] += gyro_z
+		
+		# Calculate accelerometer angles
+		accel_roll, accel_pitch = get_accel_angle(acc_x, acc_y, acc_z)
+
+		roll_th = complementary_filter(gyro_value=gyro_x,accel_value=accel_roll,dt=dt)
+		pitch_th = complementary_filter(gyro_value=gyro_y,accel_value=accel_pitch,dt=dt)
+		# print(f"Roll Offsets: {roll_th}")
+		# print(f"Pitch Offsets: {pitch_th}")
+		roll_offsets.append(roll_th)
+		pitch_offsets.append(pitch_th)
+		
+		time.sleep(0.001)  # 1ms delay between samples
+	
+	# Compute average offsets
+	accel_offsets = {k: v / num_samples for k, v in accel_offsets.items()}
+	gyro_offsets = {k: v / num_samples for k, v in gyro_offsets.items()}
+	roll_offset = stat.mean(roll_offsets)
+	pitch_offset = stat.mean(pitch_offsets)
+
+	print("Calibration complete.")
+	print(f"Accelerometer Offsets: {accel_offsets}")
+	print(f"Gyroscope Offsets: {gyro_offsets}")
+	
+	return roll_offset, pitch_offset
--- main.py
+++ main.py
@@ -1,88 +1,101 @@
 #!/usr/bin/python3
 
+import time
 import pygame
 import Actuator as nanA
 import IMU as nanImu
-import time
 
 
 def main():
-    # Initialize Pygame
-    pygame.init()
-
-    # Set up the display
-    screen = pygame.display.set_mode((100, 100))
-
-    # Initialize IMU
-    nanImu.MPU_init()
-    dt = 0.01
 
     try:
-        #calibrate sensor => offsets
-        # accel_offsets, gyro_offsets = nanImu.calibrate_mpu6050(num_samples=2000)
+
+        # Initialize Pygame
+        print("Init pygame")
+        pygame.init()
+
+        # Set up the display
+        screen = pygame.display.set_mode((100, 100))
+        time.sleep(1)
+
+        # Initialize IMU
+        print("Init IMU")
+        nanImu.MPU_init(0x69)
+        time.sleep(1)
+
+        print("Calibrating IMU")
+        roll_offset, pitch_offset = nanImu.roll_pitch_calibration(1000)
+        print(f"Roll Offsets: {roll_offset}")
+        print(f"Pitch Offsets: {pitch_offset}")
+        time.sleep(1)
+
         
         #main loop
         running = True
+        
         while running:
             start_time = time.time()
 
-            # # Read raw accelerometer data
-            # acc_x = nanImu.read_raw_data(0x3B) - accel_offsets['x']
-            # acc_y = nanImu.read_raw_data(0x3D) - accel_offsets['y']
-            # acc_z = nanImu.read_raw_data(0x3F) - accel_offsets['z']
+            # Read raw accelerometer data
+            acc_x = nanImu.read_raw_data(0x3B)
+            acc_y = nanImu.read_raw_data(0x3D)
+            acc_z = nanImu.read_raw_data(0x3F)
             
-            # # Read raw gyroscope data
-            # gyro_x = nanImu.read_raw_data(0x43) - gyro_offsets['x']
-            # gyro_y = nanImu.read_raw_data(0x45) - gyro_offsets['y']
-            # gyro_z = nanImu.read_raw_data(0x47) - gyro_offsets['z']
+            # Read raw gyroscope data
+            gyro_x = nanImu.read_raw_data(0x43)
+            gyro_y = nanImu.read_raw_data(0x45)
+            gyro_z = nanImu.read_raw_data(0x47)
             
-            # # Convert gyroscope data to degrees per second
-            # gyro_x = gyro_x / 131.0
-            # gyro_y = gyro_y / 131.0
-            # gyro_z = gyro_z / 131.0
+            # Convert gyroscope data to degrees per second
+            gyro_x = gyro_x / 131.0
+            gyro_y = gyro_y / 131.0
+            gyro_z = gyro_z / 131.0
             
-            # # Calculate accelerometer angles
-            # accel_roll, accel_pitch = nanImu.get_accel_angle(acc_x, acc_y, acc_z)
+            # Calculate accelerometer angles
+            accel_roll, accel_pitch = nanImu.get_accel_angle(acc_x, acc_y, acc_z)
+            dt = 0.08
+            roll_th = nanImu.complementary_filter(gyro_value=gyro_x,accel_value=accel_roll,dt=dt)
+            pitch_th = nanImu.complementary_filter(gyro_value=gyro_y,accel_value=accel_pitch,dt=dt)
 
-            # roll_th = nanImu.complementary_filter(gyro_value=gyro_x,accel_value=accel_roll,dt=dt)
-            # pitch_th = nanImu.complementary_filter(gyro_value=gyro_y,accel_value=accel_pitch,dt=dt)
-            # print(f"Roll: {roll_th:.2f}°, Pitch: {pitch_th:.2f}°")
+            print(f"Roll: {(roll_th - roll_offset):.2f}°, Pitch: {(pitch_th - pitch_offset):.2f}°")
             
-            for event in pygame.event.get():
+            # for event in pygame.event.get():
 
-                if event.type == pygame.QUIT:
-                    running = False
-                elif event.type == pygame.KEYDOWN:
+            #     if event.type == pygame.QUIT:
+            #         running = False
+            #     elif event.type == pygame.KEYDOWN:
 
-                    if event.key == pygame.K_UP:
-                        nanA.move_robot('forward', nanA.current_speed)
+            #         if event.key == pygame.K_UP:
+            #             nanA.move_robot('forward', nanA.current_speed)
                     
-                    elif event.key == pygame.K_DOWN:
-                        nanA.move_robot('backward', nanA.current_speed)
+            #         elif event.key == pygame.K_DOWN:
+            #             nanA.move_robot('backward', nanA.current_speed)
                     
-                    elif event.key == pygame.K_LEFT:
-                        nanA.move_robot('left', nanA.current_speed)
+            #         elif event.key == pygame.K_LEFT:
+            #             nanA.move_robot('left', nanA.current_speed)
                     
-                    elif event.key == pygame.K_RIGHT:
-                        nanA.move_robot('right', nanA.current_speed)
+            #         elif event.key == pygame.K_RIGHT:
+            #             nanA.move_robot('right', nanA.current_speed)
                     
-                    elif event.key == pygame.K_RETURN:
-                        nanA.move_robot('spin', nanA.current_speed)    
+            #         elif event.key == pygame.K_RETURN:
+            #             nanA.move_robot('spin', nanA.current_speed)    
                     
-                    elif event.key >= 48 and event.key <= 57:
-                        nanA.current_speed = (event.key - 48 ) * 10
-                        if nanA.current_speed == 0:
-                            nanA.current_speed = 100
-                        print("Speed @" + str(nanA.current_speed) + " %")
-                    elif event.key == pygame.K_PERIOD:
-                        running = False
+            #         elif event.key >= 48 and event.key <= 57:
+            #             nanA.current_speed = (event.key - 48 ) * 10
+            #             if nanA.current_speed == 0:
+            #                 nanA.current_speed = 100
+            #             print("Speed @" + str(nanA.current_speed) + " %")
+            #         elif event.key == pygame.K_PERIOD:
+            #             running = False
 
-                elif event.type == pygame.KEYUP:
-                    nanA.stop_robot()
-                time.sleep(0.001)  # To avoid high CPU usage
+            #     elif event.type == pygame.KEYUP:
+            #         nanA.stop_robot()
+                # time.sleep(0.001)  # To avoid high CPU usage
             
-            elapsed_time = time.time() - start_time
-            time_to_sleep = dt - elapsed_time
+            # elapsed_time = time.time() - start_time
+            # time_to_sleep = dt - elapsed_time
+            # print(time_to_sleep)
+            time.sleep(0.1)
             
             # if time_to_sleep > 0:
             #     time.sleep(time_to_sleep)
